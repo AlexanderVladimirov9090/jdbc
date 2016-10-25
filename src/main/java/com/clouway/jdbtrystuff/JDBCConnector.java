ae0982325d4b5daf8575df8f4ca2289a91b5893f
Alexander Vladimirov <alexandervladimirov1902@gmail.com>
2016-10-25 14:34:24 +0300

    Initial Commit.

---
+++ src/main/java/com/clouway/jdbtrystuff/JDBCConnector.java
@@ -0,0 +1,29 @@
+package com.clouway.jdbtrystuff;
+
+import java.sql.*;
+
+/**
+ * Created by clouway on 25.10.16.
+ *
+ * @author Alexander Vladimirov
+ *         <alexandervladimirov1902@gmail.com>
+ */
+public class JDBCConnector {
+    private final String JDBC_DRIVER;
+    private final String DB_URL;
+    private final String USER;
+    private final String PASSWORD;
+
+    public JDBCConnector(String jDBCDriver, String databaseURL, String user, String password) {
+        this.JDBC_DRIVER = jDBCDriver;
+        this.DB_URL = databaseURL;
+        this.USER = user;
+        this.PASSWORD = password;
+    }
+
+    public Connection getConnection() throws SQLException, ClassNotFoundException {
+        Class.forName(JDBC_DRIVER);
+        Connection connection = DriverManager.getConnection(DB_URL,USER,PASSWORD);
+        return connection;
+    }
+}
